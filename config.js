41247000087cd6f965a9f949b8ec0188adbd3976
divrawat <divrawat2001@gmail.com>
2023-11-02 19:29:34 +0530

    88888

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-//  export const API = "https://whiskyprices-backend.vercel.app/api";
-//  export const DOMAIN = "https://stories.whiskyprices.ninja";
+export const API = "https://whiskyprices-backend.vercel.app/api";
+export const DOMAIN = "https://stories.whiskyprices.ninja";
 export const MY_API="https://whiskyprices-backend.vercel.app";
 
 export const APP_NAME = "Whiskyprices";
 export const DISQUS_SHORTNAME = "my-cms-7";
 export const APP_DESCRIPTION = "Whiskyprices is a blog website where users can publish their articles related to programming or computer science field. It is designed to help individuals learn programming concepts, languages, and best practices."
 
- export const API = 'http://localhost:8000/api';
- export const DOMAIN = "http://localhost:3000";
+//  export const API = 'http://localhost:8000/api';
+//  export const DOMAIN = "http://localhost:3000";
